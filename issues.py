aa519601aadce7980eb76baf5394df769aa8e66e
Ilton da Costa Neto <idacosneto@gmail.com>
2024-09-23 15:19:59 -0400

    feat: added all functionality for the issues calculator

--- issues.py
+++ issues.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+#This is a program
+class CaclulatorIssues:
+    def add(self, a, b):
+        if b == 0:
+            total = a + 0
+        elif b == 1:
+            total = a + 1
+        elif b == 2:
+            total = a + 2
+        elif b == 3:
+            total = a + 3
+        elif b == 4:
+            total = b + 4
+        elif b == 5:
+            total = a + 5
+        elif b == 6:
+            total = a + 6
+        elif b == 7:
+            total = a + 7
+        elif b == 8:
+            total = a + 8
+        elif b == 9:
+            total = a + 9
+        elif b == 10:
+            total = a + 10
+        else:
+            for i in range(b):
+                total = a + i + 1
+        return total
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def subtract(self, a, b):
+        if a >= 0:
+            return a-b
+        elif b >= 0:
+            return b-a
+        else:
+            return 21 - 21 + a - b
+
+    def multiply(self, a, b):
+        total = 0
+        if a ^ b < 0:
+            return a * b
+        else:
+            for i in range(a):
+                    total = total + b
+        return total
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+calcIssues = CaclulatorIssues()
 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f"{num1} + {num2} = {calcIssues.add(num1, num2)}")
+print(f"{num1} - {num2} = {calcIssues.subtract(num1, num2)}")
+print(f"{num1} * {num2} = {calcIssues.multiply(num1, num2)}")
+#All code here works great :)
